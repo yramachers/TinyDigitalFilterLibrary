46d0cc4da8af7f49c702b55861cb8fdd9ec696dc
Yorck Ramachers <y.a.ramachers@warwick.ac.uk>
2023-03-04 14:11:08 +0000

    [test] added BW filter to test demo. Runs, no check yet.

--- test/testdfilter.cpp
+++ test/testdfilter.cpp
@@ -11,6 +11,8 @@
 
 int main() {
   void datasource(std::vector<double> &t, std::vector<double> &d);
+  void oscillationsource(std::vector<double> &d);
+  TH1D* testbworth(std::vector<double>& data);  
   TH1D* testmav(std::vector<double>& data);
   TH1D* testLP(std::vector<double>& data);
   TH1D* testHP(std::vector<double>& data);
@@ -23,10 +25,16 @@ int main() {
 
   std::vector<double> stde;
   std::vector<double> data;
+  std::vector<double> osc;
   datasource(stde, data);
+  oscillationsource(osc);
 
   std::cout << "Made pulse, ndata = " << data.size() << std::endl;
   std::cout << "Made stde size = " << stde.size() << std::endl;
+  std::cout << "Made oscillation size = " << osc.size() << std::endl;
+
+  // test moving average filter
+  TH1D* bwhist = testbworth(osc);
 
   // test moving average filter
   TH1D* mavhist = testmav(data);
@@ -50,8 +58,16 @@ int main() {
     dhist->SetBinContent(bin, entry);
     bin++;
   }
+  bin = 0;
+  TH1D* ohist = new TH1D("oscd","oscillation",osc.size(),0,osc.size()-1);
+  for (double& entry : osc) {
+    ohist->SetBinContent(bin, entry);
+    bin++;
+  }
 
   dhist->Write();
+  ohist->Write();
+  bwhist->Write();
   mavhist->Write();
   lrchist->Write();
   hrchist->Write();
@@ -60,6 +76,25 @@ int main() {
   ff->Close();
   return 0;
 }
+
+TH1D* testbworth(std::vector<double>& data) {
+  DFButterworth lrc;
+  std::cout << "Instantiated DFButterworth." << std::endl;
+
+  // single pass filter is default
+  lrc.SetSamplingTimeBase(1.e-3); // 1 kHz; default, 1 ns
+  lrc.SetLowFilterFreq(1.0e2);  // 100 Hz
+
+  std::vector<double> newosc = lrc.Filter(data);  
+  std::cout << "Butterworth executed" << std::endl;
+
+  int nf = newosc.size();
+  TH1D* fhist = new TH1D("bwf","filtered pulse",nf,0,nf-1);
+  for (int i=0;i<nf;i++) 
+    fhist->SetBinContent(i,newosc[i]);
+  return fhist;
+}
+
 
 TH1D* testmav(std::vector<double>& data) {
   DFMovingAverage mav;
@@ -158,6 +193,18 @@ TH1D* testmatch(std::vector<double>& data, std::vector<double>& st) {
   for (int i=0;i<nf;i++) 
     fhist->SetBinContent(i,newpulse[i]);
   return fhist;
+}
+
+
+void oscillationsource(std::vector<double> &osc) {
+  int samplelength = 10000; // 10 s at 1 kHz
+  double f[4] = {25,110,150,250}; // frequencies [Hz]
+  const double tpi = 2.0*std::acos(-1.0);
+  for (int i=0;i<samplelength;++i)
+    osc.push_back(std::sin(tpi*f[0]*i*10/samplelength)
+		  +std::sin(tpi*f[1]*i*10/samplelength)
+		  +std::sin(tpi*f[2]*i*10/samplelength)
+		  +std::sin(tpi*f[3]*i*10/samplelength));
 }
 
 
